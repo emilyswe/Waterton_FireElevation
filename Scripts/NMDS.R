ccf01d3976cfa653058844b7be9a517ee2289638
Emily Swerfager <emilyswe@ualberta.ca>
2024-03-13 17:49:52 -0600

    cleaning nmds matrix and updating script using standardized, cleaned data

--- Scripts/NMDS.R
+++ Scripts/NMDS.R
@@ -4,9 +4,24 @@ library(vegan)
 
 # Set seed for reproducibility
 set.seed(2)
+# Import your community matrix from the CSV file
+community_matrix_raw <- read.csv("Input/04_Cleaned_Waterton.csv")
+
+# Need to extract only species data columns 
+# Assuming species data is from column 6 to 116. 
+community_matrix <- community_matrix_raw[, 6:116] # Adjust X to the index for column DL
+
+# Retain the site information for plotting. Assuming it's in the fourth column, adjust if it's different.
+site_info <- community_matrix_raw[, 4]
+
+# Convert any non-numeric columns in the community matrix to numeric 
+community_matrix <- data.frame(lapply(community_matrix, as.numeric))
+
+#save matrix
+write_csv(community_matrix, "Input/05_Cleaned_Matrix.csv")
 
 # Import your community matrix from the CSV file you exported
-community_matrix <- read.csv("~/Documents/local-git/Waterton/Output/Wisconsin_Scaled_Matrix.csv", row.names = 1)
+community_matrix <- read.csv("Input/05_Cleaned_Matrix.csv")
 
 # Run NMDS with your community matrix, specifying Bray-Curtis dissimilarity
 # and setting the number of attempts to find a stable solution to 100
